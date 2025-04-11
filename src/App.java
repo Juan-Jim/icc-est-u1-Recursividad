fc16740f7a8b0a0dd13f3c68f77b9ec89f72ad9a
Juan <juandajimenezcriollo@gmail.com>
2025-04-10 19:15:34 -0500

    Trabajo en clase

--- src/App.java
+++ src/App.java
@@ -1,10 +1,22 @@
 public class App {
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
         int n = 5;
         int resultadoFinal = factorial(n);
         System.out.println("Factorial de " + n + ": " + resultadoFinal);
         activar(n);
+
+        int base = 2;
+        int exponente = 3;
+
+        EjercicioRecursividada potencia = new EjercicioRecursividada();
+        int resultado = potencia.elevado(base, exponente);
+        System.out.println("la potencia de: " + base + " es: " + resultado);
+
+        int numero = 124;
+        EjercicioRecursividada ultimo = new EjercicioRecursividada();
+        int resultadonumero = ultimo.devolver(numero);
+        System.out.println("El resultado final es: " + resultadonumero);
     }
     public static int factorial(int n) {
         if (n == 0) {
@@ -21,4 +33,7 @@ public class App {
         int resultadoSuma = ejercicio.sumaConsecutivos(n);
         System.out.println("Suma de 1 hasta " + n + ": " + resultadoSuma);
     }
+
+    
 }
+
--- src/EjercicioRecursividada.java
+++ src/EjercicioRecursividada.java
@@ -10,11 +10,29 @@ public class EjercicioRecursividada {
     public int sumaConsecutivos(int n) {
         if (n==1) {
             return 1;
-        }
+        } 
             int resultadoPracial = sumaConsecutivos(n-1);
             int resultado = n + resultadoPracial;
             return resultado;
     }
     
+    public  int elevado(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        }else{
+
+            return base * elevado(base ,exponente -1 );
+        }
+    }
     
+    public int devolver (int numero ) {
+        if (numero < 10) {
+            return numero;
+        }else{
+            int ultimoNumero = numero % 10;
+            int restaNumero = numero / 10;
+            return  ultimoNumero + devolver(restaNumero);
+        }
+        
+    } 
 }
